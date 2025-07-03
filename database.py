facb5570be900f52fc18db5ac5c363643e6d1cae
Chigurubattula Jatin <125136867+Jatinch05@users.noreply.github.com>
2025-07-03 18:19:56 +0530

    Update database.py

--- database.py
+++ database.py
@@ -7,7 +7,8 @@ class Database:
             host=os.getenv("DB_HOST", "localhost"),
             user=os.getenv("DB_USER", "root"),
             password=os.getenv("DB_PASSWORD", ""),
-            database=os.getenv("DB_NAME", "benchmark")
+            database=os.getenv("DB_NAME", "benchmark"),
+            port=int(os.getenv("DB_PORT", 3306))
         )
         self.cursor = self.conn.cursor()
 
